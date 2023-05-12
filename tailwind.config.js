68882a02690584397a587b8b767876153a0b646b
Akshat Srivastava <akshat_tln@yahoo.com>
2023-05-12 13:53:09 +0530

    July version

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "*.{html,js}",
+    "/index.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'rubik': ['Rubik', 'sans-serif'] 
+      },
+      colors: {
+        'primary': '#7b2cbf',
+        'secondary': '#6712b0',
+        'dark': '#131b1e'
+      }
+    },
+  },
+  plugins: [],
+}
